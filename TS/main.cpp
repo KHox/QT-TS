f9f5eaf584f6e23160564323bb4eed59c8387de7
KHox <56696630+KHox@users.noreply.github.com>
2023-06-16 23:47:48 +0500

    updated

--- TS/main.cpp
+++ TS/main.cpp
@@ -42,9 +42,9 @@ public:
             {
                 int stb_pos = 0;
                 int m = INT_MAX;
-                for (const auto &i:*it)
+                for (const int &i:*it)
                 {
-                    if(i < m && !isInVector(stbs, stb_pos)) m = i;
+                    if(i < m && i != 0 && !isInVector(stbs, stb_pos)) m = i;
                     ++stb_pos;
                 };
                 sum += m;
@@ -66,7 +66,7 @@ public:
                 int m = INT_MAX;
                 for (auto i = matrix.begin(); i != matrix.end(); ++i)
                 {
-                    if((*i)[stb_pos] < m && !isInVector(strs, str_pos)) m = (*i)[stb_pos];
+                    if((*i)[stb_pos] < m && (*i)[stb_pos] != 0 && !isInVector(strs, str_pos)) m = (*i)[stb_pos];
                     ++str_pos;
                 };
                 sum += m;
@@ -205,6 +205,7 @@ pair<int, vector<int>> solveTravelingSalesmanProblem(const vector<vector<int>>& 
 
     while (true)
     {
+        qDebug() << v;
         list<BranchState>::iterator itr = cont.nodes.begin();
         list<BranchState> new_nodes;
         int i = 0;
@@ -241,6 +242,28 @@ public:
         connect(addEdgeButton, &QPushButton::clicked, this, &GraphWidget::addEdge);
         connect(removeEdgeButton, &QPushButton::clicked, this, &GraphWidget::removeEdge);
         connect(calculateButton, &QPushButton::clicked, this, &GraphWidget::calculate);
+        connect(showMatrix, &QPushButton::clicked, this, &GraphWidget::ShowM);
+    }
+
+    void ShowM()
+    {
+        QString str = "";
+        for (int i = 0 ; i < int(graph.size()); i++)
+        {
+            for (int j = 0 ; j < int(graph.size()); j++)
+            {
+                if (i == j)
+                {
+                    str += "- ";
+                }
+                else
+                {
+                    str += QString::number(graph[i][j]) + " ";
+                }
+            }
+            str += "\n";
+        }
+        QMessageBox::information(this, "Матрица", str);
     }
 
     void addVertex() {
@@ -256,7 +279,7 @@ public:
     void removeVertex() {
         bool ok;
         int vertexIndex = QInputDialog::getInt(this, tr("Удалить вершину"), tr("Введите индекс вершины:"), 0, 0, INT_MAX, 1, &ok);
-        if (ok && vertexIndex >= 0 && vertexIndex <= graph.size()) {
+        if (ok && vertexIndex >= 0 && vertexIndex <= int(graph.size())) {
             graph.erase(graph.begin() + vertexIndex);
             for (auto& row : graph) {
                 row.erase(row.begin() + vertexIndex);
@@ -268,14 +291,17 @@ public:
     void addEdge() {
         bool ok;
         int vertexIndex1 = QInputDialog::getInt(this, tr("Добавить ребро"), tr("Введите индекс первой вершины:"), 0, 0, INT_MAX, 1, &ok);
-        if (ok && vertexIndex1 >= 0 && vertexIndex1 <= graph.size()) {
+        if (ok && vertexIndex1 >= 0 && vertexIndex1 <= int(graph.size())) {
             int vertexIndex2 = QInputDialog::getInt(this, tr("Добавить ребро"), tr("Введите индекс второй вершины:"), 0, 0, INT_MAX, 1, &ok);
-            if (ok && vertexIndex2 >= 0 && vertexIndex2 <= graph.size()) {
-                int weight = QInputDialog::getInt(this, tr("Добавить ребро"), tr("Введите вес ребра:"), 0, 0, INT_MAX, 1, &ok);
+            if (ok && vertexIndex2 >= 0 && vertexIndex2 <= int(graph.size())) {
+                int weight = QInputDialog::getInt(this, tr("Добавить ребро"), tr("Введите вес ребра из 1 вершины во 2:"), 0, 0, INT_MAX, 1, &ok);
                 if (ok) {
-                    graph[vertexIndex1][vertexIndex2] = weight;
-                    graph[vertexIndex2][vertexIndex1] = weight;
-                    update();
+                    int weight2 = QInputDialog::getInt(this, tr("Добавить ребро"), tr("Введите вес ребра из 2 вершины в 1:"), weight, 0, INT_MAX, 1, &ok);
+                    if (ok) {
+                        graph[vertexIndex1][vertexIndex2] = weight;
+                        graph[vertexIndex2][vertexIndex1] = weight2;
+                        update();
+                    }
                 }
             }
         }
@@ -317,19 +343,30 @@ public:
         double R = 2 * M_PI / int(graph.size());
 
         for (int i = 0; i < int(graph.size()); ++i) {
-            for (int j = i + 1; j < int(graph.size()); ++j) {
+            for (int j = 0; j < int(graph.size()); ++j) {
                 if (graph[i][j] != 0) {
                     double x1 = qCos(i * R) * bR;
                     double y1 = qSin(i * R) * bR;
                     double x2 = qCos(j * R) * bR;
                     double y2 = qSin(j * R) * bR;
+
                     double dx = x2 - x1;
                     double dy = y2 - y1;
+
                     double vlen = qSqrt(dx * dx + dy * dy);
+
+                    double kx = dy / vlen * radius;
+                    double ky = -dx / vlen * radius;
+
                     double dx2 = dy / vlen * tS / 2;
                     double dy2 = -dx / vlen * tS / 2;
-                    painter.drawLine(QPointF(x1 + dx / vlen * radius, y1 + dy / vlen * radius), QPointF(x2 - dx / vlen * radius, y2 - dy / vlen * radius));
-                    painter.drawText(QRectF((x1 + x2) / 2 + dx2 - tS / 2 , (y1 + y2) / 2 + dy2 - tS / 2, tS, tS), Qt::AlignCenter, QString::number(graph[i][j]));
+
+                    painter.drawLine(QPointF(x1 - kx, y1 - ky), QPointF(x2 - kx, y2 - ky));
+
+                    painter.drawLine(QPointF(x2 - kx + dx2 / 2 + dy2, y2 - ky + dy2 / 2 - dx2), QPointF(x2 - kx, y2 - ky));
+                    painter.drawLine(QPointF(x2 - kx - dx2 / 2 + dy2, y2 - ky - dy2 / 2 - dx2), QPointF(x2 - kx, y2 - ky));
+
+                    painter.drawText(QRectF((x1 + x2) / 2 - kx - dx2 - tS / 2 , (y1 + y2) / 2 - ky - dy2 - tS / 2, tS, tS), Qt::AlignCenter, QString::number(graph[i][j]));
                 }
             }
         }
@@ -342,39 +379,38 @@ public:
             painter.drawText(QRectF(x - radius, y - radius, radius * 2, radius * 2), Qt::AlignCenter, QString::number(i));
         }
 
-        minDistance = 0;
+        vector<int> path;
+
         for (int i = 0; i < int(minPath.size()); i++)
         {
-            if (i + 1 == int(minPath.size()))
-            {
-                minDistance += graph[minPath[i]][minPath[0]];
-            }
-            else
-            {
-                minDistance += graph[minPath[i]][minPath[i + 1]];
-            }
+            path.push_back(minPath[i]);
+        }
+        path.push_back(0);
+
+        minDistance = 0;
+        for (int i = 0; i < int(path.size()) - 1; i++)
+        {
+
+            minDistance += graph[path[i]][path[i + 1]];
         }
 
         painter.drawText(QRectF(-250, -250, 300, 30), Qt::AlignLeft, "Минимальная дистанция: " + QString::number(minDistance));
         QString pathText = "Кратчайший путь: ";
-        for (int i = 0; i < int(minPath.size()); ++i) {
-            pathText += QString::number(minPath[i]);
-            if (i != int(minPath.size()) - 1) {
+
+
+
+        for (int i = 0; i < int(path.size()); ++i) {
+            pathText += QString::number(path[i]);
+            if (i != int(path.size()) - 1) {
                 pathText += " -> ";
             }
         }
-
-        if (int(minPath.size()) > 1)
-        {
-            pathText += " -> 0";
-        }
-
         painter.drawText(QRectF(-250, -220, 300, 30), Qt::AlignLeft, pathText);
 
         painter.setPen(Qt::red);
-        for (int i = 0; i < int(minPath.size()) - 1; ++i) {
-            int vertex1 = minPath[i];
-            int vertex2 = minPath[i + 1];
+        for (int i = 0; i < int(path.size()) - 1; ++i) {
+            int vertex1 = path[i];
+            int vertex2 = path[i + 1];
 
             double x1 = qCos(vertex1 * R) * bR;
             double y1 = qSin(vertex1 * R) * bR;
@@ -384,15 +420,19 @@ public:
             double dx = x2 - x1;
             double dy = y2 - y1;
             double vlen = qSqrt(dx * dx + dy * dy);
-            painter.drawLine(QPointF(x1 + dx / vlen * radius, y1 + dy / vlen * radius), QPointF(x2 - dx / vlen * radius, y2 - dy / vlen * radius));
-        }
 
+            double kx = dy / vlen * radius;
+            double ky = -dx / vlen * radius;
+            painter.drawLine(QPointF(x1 - kx, y1 - ky), QPointF(x2 - kx, y2 - ky));
+        }
     }
+
     QPushButton* addVertexButton = new QPushButton("Добавить вершину");
     QPushButton* removeVertexButton = new QPushButton("Удалить вершину");
     QPushButton* addEdgeButton = new QPushButton("Добавить ребро");
     QPushButton* removeEdgeButton = new QPushButton("Удалить ребро");
     QPushButton* calculateButton = new QPushButton("Просчитать");
+    QPushButton* showMatrix = new QPushButton("Показать матрицу");
 private:
     vector<vector<int>> graph; // Матрица смежности
     int minDistance; // Минимальное расстояние
@@ -417,6 +457,7 @@ int main(int argc, char** argv) {
     layout->addWidget(graphWidget.addEdgeButton);
     layout->addWidget(graphWidget.removeEdgeButton);
     layout->addWidget(graphWidget.calculateButton);
+    layout->addWidget(graphWidget.showMatrix);
 
     QWidget widget;
     widget.setLayout(layout);
